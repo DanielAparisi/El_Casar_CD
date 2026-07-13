bfecbac76bf49040fba39684697b99e77df50377
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-07-13 23:03:57 +0200

    Conncetin g POSTGRESSQL to java logic

--- src/main/java/com/ElCasarCD/web/model/ListaJugadores.java
+++ src/main/java/com/ElCasarCD/web/model/ListaJugadores.java
@@ -1,5 +1,22 @@
 package com.ElCasarCD.web.model;
 
 public class ListaJugadores {
+
+
     private Jugador listaJugadores[];
+    public int numJugadores;
+
+    public static final int EXITO = 0;
+    public static final int ERROR_JugadoNULL = 1;
+    public static final int ERROR_DEMASIADOS_JUGADORES = 2;
+
+    public ListaJugadores(Jugador[] listaJugadores, int numJugadores) {
+        this.listaJugadores = listaJugadores;
+        this.numJugadores = numJugadores;
+    }
+
+    
+
+
+    
 }
