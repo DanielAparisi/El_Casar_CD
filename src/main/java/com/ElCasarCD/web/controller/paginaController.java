e880dfcf88d2c971667250a720ef58140cee59b1
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-07-13 18:26:25 +0200

    web entry root fixed

--- src/main/java/com/ElCasarCD/web/controller/paginaController.java
+++ src/main/java/com/ElCasarCD/web/controller/paginaController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class paginaController {
     @GetMapping("/")
     public String index() {
-        return "Index";
+        return "index";
     }
 }
